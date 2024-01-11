252ee1696c6d9f84ca05b84a83f451a1b8374dd4
zakaria0101 <zraissi403@gmail.com>
2024-01-12 00:21:16 +0100

    second

--- src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "Hello from test test :) ✨";
+        return "Hello from test test ... :) ✨";
     }
 
     @GetMapping("/user")
